0ed2a6956b4ff6157c4237d5b72b889556829090
iLauncherDev <tvistaifari@gmail.com>
2023-11-09 14:16:15 -0300

    .

--- bios/bios.asm
+++ bios/bios.asm
@@ -5,6 +5,8 @@ extern main
 
 start:
     mov sp, 0x3000
+    mov ecx, 0xdeadc0de
+    movzx eax, cx
     call main
 .loop:
     jmp .loop
--- bios/main.c
+++ bios/main.c
@@ -11,9 +11,11 @@ void print(char *string)
 void main()
 {
     char string_test[] = "iLauncherBios\n";
+    memcpy((void *)0xf000, string_test, sizeof(string_test));
     while (true)
     {
         memset((void *)inl(0xfff0), 0xff, inw(0xfff4) * inw(0xfff6) * inw(0xfff8));
         memset((void *)inl(0xfff0), 0x00, inw(0xfff4) * inw(0xfff6) * inw(0xfff8));
+        print((void *)0xf000);
     }
 }
