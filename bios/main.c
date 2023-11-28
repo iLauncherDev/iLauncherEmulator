ae1618ba883562ada39e5306cdb5ec06408d59ec
Fiusk89 <136613858+Fiusk89@users.noreply.github.com>
2023-11-28 17:24:09 -0300

    added new opcodes (#21)
    * new opcodes
    
    * .

--- bios/main.c
+++ bios/main.c
@@ -8,10 +8,31 @@ void print(char *string)
         outb(0x3f8, string[i]);
 }
 
+void putpixel(uint32_t x, uint32_t y, uint32_t color)
+{
+    uint16_t width = inw(0xfff4), height = inw(0xfff6);
+    if (x >= width)
+        x = width;
+    if (y >= height)
+        y = height;
+    switch (inb(0xfff8))
+    {
+    case 1:
+        ((uint8_t *)inl(0xfff0))[y * width + x] = color;
+        break;
+    case 2:
+        ((uint16_t *)inl(0xfff0))[y * width + x] = color;
+        break;
+    case 4:
+        ((uint32_t *)inl(0xfff0))[y * width + x] = color;
+        break;
+    }
+}
+
 void main()
 {
     uint32_t *buffer = (void *)inl(0xfff0);
-    uint32_t size = inw(0xfff4) * inw(0xfff6) * inw(0xfff8);
+    uint32_t size = inw(0xfff4) * inw(0xfff6) * inb(0xfff8);
     char string_test[] = "iLauncherBios\n";
     memcpy((void *)0xf000, string_test, sizeof(string_test));
     while (true)
